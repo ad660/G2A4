806b2621f4b5233fc2e6309c4c7e933a586e6ea6
JaneNeave <janeneave101@gmail.com>
2023-10-23 23:56:39 +0100

    reorganising db_utils

--- db_utils.py
+++ db_utils.py
@@ -1,21 +1,6 @@
 import mysql.connector
 from config import HOST, USER, PASSWORD
 
-import mysql.connector
-
-def RunQuery():
-    conn=mysql.connector.connect(host="localhost",user="root",password="Zenjie12!",database="Hogwarts_Library")
-    connection=conn.cursor()
-    connection.execute("select * from books")
-    results=connection.fetchall()
-    items=[]
-    for row in results:
-        items.append({'title':row[1],'author': row[2],'year_published':row[3],'subject':row[4],'description':row[5],'age_restrict':row[6],'stockID':row[7]})
-
-
-
-    return items
-
 def _connect_to_db(db_name):
     cnx = mysql.connector.connect(
         host=HOST,
@@ -25,6 +10,16 @@ def _connect_to_db(db_name):
         database=db_name
     )
     return cnx
+
+def RunQuery():
+    conn=mysql.connector.connect(host="localhost",user="root",password="Zenjie12!",database="Hogwarts_Library")
+    connection=conn.cursor()
+    connection.execute("select * from books")
+    results=connection.fetchall()
+    items=[]
+    for row in results:
+        items.append({'title':row[1],'author': row[2],'year_published':row[3],'subject':row[4],'description':row[5],'age_restrict':row[6],'stockID':row[7]})
+    return items
 
 
 ## GET BOOKS ON LOAN BY STUDENT ID
